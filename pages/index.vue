fddea7acab7cd724ce50d905510a7c3d62243322
redsky0212 <hyun0238@gmail.com>
2020-08-04 09:15:44 +0900

    vi테스트 커밋

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
   <div class="container">
     <header>
       <h1 class="title"> {{ title }} </h1>
+      <h2>{{test}}</h2>
       <nav>
         <nuxt-link to="/services"> 서비스 이름 </nuxt-link>
       </nav>
@@ -13,7 +14,8 @@
 export default {
   data () {
     return  { 
-      title: '프로젝트 메인 타이틀' 
+      title: '프로젝트 메인 타이틀',
+      test: 'vi테스트',
     }
   }
 }
