356eac0716461fc387fe01979a0ba0d8b2b22d48
redsky <hyun0238@gmail.com>
2020-08-05 23:04:34 +0900

    메뉴추가 및 레이아웃파일 추가

---
+++ client/layouts/default.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <header-bar />
+    <div class="main-container ace-save-state" id="main-container">
+      <side-bar />
+      <div class="main-content">
+        <div class="main-content-inner">
+          <bread-crumbs />
+          <div class="page-content">
+            <nuxt />
+          </div>
+        </div>
+      </div>
+      <footer-bar />
+    </div>
+  </div>
+</template>
+
+<script>
+import HeaderBar from './header/HeaderBar.vue';
+import SideBar from './side/SideBar.vue';
+import BreadCrumbs from './etc/BreadCrumbs.vue';
+import FooterBar from './footer/FooterBar.vue';
+
+export default {
+  components: {
+    HeaderBar,
+    SideBar,
+    BreadCrumbs,
+    FooterBar,
+  },
+};
+</script>
---
+++ client/layouts/header/HeaderBar.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="navbar" class="navbar navbar-default navbar-fixed-top ace-save-state">
+    <div class="navbar-container ace-save-state" id="navbar-container">
+      <button type="button" class="navbar-toggle menu-toggler pull-left" id="menu-toggler" data-target="#sidebar">
+        <span class="sr-only">Toggle sidebar</span>
+
+        <span class="icon-bar"></span>
+
+        <span class="icon-bar"></span>
+
+        <span class="icon-bar"></span>
+      </button>
+
+      <div class="navbar-header pull-left">
+        <router-link to="/" class="navbar-brand">
+          <small>
+            <i class="fab fa-vuejs"></i>
+            Guide
+          </small>
+        </router-link>
+      </div>
+
+      <div class="nav-search" id="nav-search">
+        <form class="form-search">
+          <span class="input-icon">
+            <input
+              type="text"
+              placeholder="Search ..."
+              class="nav-search-input"
+              id="nav-search-input"
+              autocomplete="off"
+            />
+            <i class="ace-icon fa fa-search nav-search-icon"></i>
+          </span>
+        </form>
+      </div>
+      <!-- /.nav-search -->
+    </div>
+    <!-- /.navbar-container -->
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.nav-search {
+  padding-top: 8px;
+}
+.nav-search-input {
+  border: 0;
+}
+</style>
